fed0009e35c260e1f03f8a0e5df0e8902f7b272a
Ashwini501 <ashwinitnevase@gmail.com>
2023-04-04 15:29:30 +0530

    Project structure created

---
+++ Architecture/app/src/main/java/com/pocproject/architecture/Home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.pocproject.architecture.Home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pocproject.architecture.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
