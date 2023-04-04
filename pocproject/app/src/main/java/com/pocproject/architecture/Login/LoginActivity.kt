afb6bf250573088ae918c1dea67306f93c4626a4
Ashwini501 <ashwinitnevase@gmail.com>
2023-04-04 14:21:36 +0530

    Project structure created

---
+++ pocproject/app/src/main/java/com/pocproject/architecture/Login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.pocproject.architecture.Login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pocproject.architecture.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
